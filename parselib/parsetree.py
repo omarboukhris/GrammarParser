41103f3cb3af7d361ff4d6e7fc984b64318330db
Omar Boukhris <omarboukhris@think-it.io>
2019-03-20 22:22:00 +0100

    added debug mode

--- parselib/parsetree.py
+++ parselib/parsetree.py
@@ -1,4 +1,4 @@
-DEBUG=True
+DEBUG=False
 
 class UnitNode :
 	def __init__ (self, unit, nodetype) :
@@ -9,13 +9,22 @@ class UnitNode :
 		return self.nodetype.find("/") != -1
 
 	def unfold(self, parent=None):
-		if self.iscompacted() or parent == self.nodetype :
-			return self.unit.unfold(self.nodetype)
-		else :#if parent != None :
-			return " {} = [\n {} \n]".format(
-				self.nodetype,
-				self.unit.unfold(self.nodetype),
-			)
+		if DEBUG :
+			if self.iscompacted() or parent == self.nodetype :
+				return self.unit.unfold(self.nodetype)
+			else :#if parent != None :
+				return " {} = [\n {} \n]".format(
+					self.nodetype,
+					self.unit.unfold(self.nodetype),
+				)
+		else :
+			if self.iscompacted() or parent == self.nodetype :
+				return self.unit.unfold(self.nodetype)
+			else :#if parent != None :
+				return " {} = [ {} ]".format(
+					self.nodetype,
+					self.unit.unfold(self.nodetype),
+				)
 
 	def __str__ (self) :
 		return self.nodetype
@@ -50,17 +59,30 @@ class BinNode :
 		return self.right.nodetype == self.left.nodetype
 
 	def unfold(self, parent=None):
-		if self.iscompacted() or parent == self.nodetype : 
-			return "{} \n {}".format( # make it merge two dicts
-				self.left.unfold(self.nodetype),
-				self.right.unfold(self.nodetype),
-			)
+		if DEBUG :
+			if self.iscompacted() or parent == self.nodetype : 
+				return "{} \n {}".format( # make it merge two dicts
+					self.left.unfold(self.nodetype),
+					self.right.unfold(self.nodetype),
+				)
+			else :
+				return "{} = [\n {} \n {} \n]".format(
+					self.nodetype,
+					self.left.unfold(self.nodetype),
+					self.right.unfold(self.nodetype),
+				)
 		else :
-			return "{} = [\n {} \n {} \n]".format(
-				self.nodetype,
-				self.left.unfold(self.nodetype),
-				self.right.unfold(self.nodetype),
-			)
+			if self.iscompacted() or parent == self.nodetype : 
+				return "{} {}".format( # make it merge two dicts
+					self.left.unfold(self.nodetype),
+					self.right.unfold(self.nodetype),
+				)
+			else :
+				return "{} = [ {} {} ]".format(
+					self.nodetype,
+					self.left.unfold(self.nodetype),
+					self.right.unfold(self.nodetype),
+				)
 	
 	def __str__ (self) :
 		return self.unfold().__str__() #self.nodetype
